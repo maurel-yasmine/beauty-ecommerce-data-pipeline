f25e4ad2946a63fa671c3c0a8dc0309926ed4146
Maurel Yasmine <echamaurel@gmail.com>
2026-09-16 22:04:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ from src import etl_pipeline
 from src import run_analysis
 
 if __name__ == "__main__":
-    print("
-### 1/3 — GENERATE DATA ###")
+    print("### 1/3 - GENERATE DATA ###")
     generate_data.write_products()
     generate_data.write_orders()
 
-    print("
-### 2/3 — ETL PIPELINE ###")
+    print("### 2/3 - ETL PIPELINE ###")
     etl_pipeline.run_pipeline()
 
-    print("
-### 3/3 — ANALISIS & DASHBOARD ###")
+    print("### 3/3 - ANALISIS & DASHBOARD ###")
     run_analysis.main()
 
-    print("SEMUA SELESAI! Cek folder reports/ untuk melihat dashboard.")
+    print("SEMUA SELESAI! Cek folder reports/ untuk dashboard.")
